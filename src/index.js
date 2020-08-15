a8719506082955fbdc6030b2d04057a78f5553f8
Jose Valadez <vinmajj@gmail.com>
2020-08-15 08:26:53 -0700

    Remove create react boiler plate template

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import teams from './teams.json';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App teams={teams.result}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
