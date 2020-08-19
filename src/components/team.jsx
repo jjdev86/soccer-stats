10019a2379c27a0ee228d0ac0c3d12bcba6fb8f7
Jose Valadez <vinmajj@gmail.com>
2020-08-18 21:07:20 -0700

    create team component

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
 
 function Team({match}) {
- return (
-    <div className="match-container">
-    <div className="team-container">
-      <div className="team-container">
+    return (
+        <div className="team-container">
         <div className="team-logo"></div>
         <div className="team-sc">
-          {match.score ? (
-          <div className="team-sc-t">{match.score.ft[0]}</div>
-          ) : (
-              <div></div>
-          )}
+          <div className="team-sc-t team-text">{match.score}</div>
         </div>
         <div className="team">
-          <span>{match.team1}</span>
+          <span className="team-text">{match.team}</span>
+          <span className="a-player-card"></span>
           </div>
       </div>
-      <div>{match.team2}</div>
-      <div>{match.date}</div>
-      {match.score ? (
-        <div className="a-score">{`${match.score.ft[0]} - ${match.score.ft[1]}`}</div>
-      ) : (
-        <div></div>
-      )}
-    </div>
-    {/* <img src={team.team_logo} /> */}
-  </div>
- )
-}; 
+    )
+};
 
 export default Team;
